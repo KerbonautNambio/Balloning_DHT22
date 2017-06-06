7fa6c1809509d39200349cfb8d2d8d27a84aab67
KerbonautNambio <www.uhnambio@gmail.com>
2017-06-06 21:54:22 +0100

    Add files via upload
    A porgram that measures temperature, humidity and the dew point in real time.

---
+++ TEMP_SENSOR_with_dew_point.ino
@@ -0,0 +1,70 @@
+double dewPoint(double celsius, double humidity)
+{
+  // (1) Saturation Vapor Pressure = ESGG(T)
+  double RATIO = 373.15 / (273.15 + celsius);
+  double RHS = -7.90298 * (RATIO - 1);
+  RHS += 5.02808 * log10(RATIO);
+  RHS += -1.3816e-7 * (pow(10, (11.344 * (1 - 1 / RATIO ))) - 1) ;
+  RHS += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1) ;
+  RHS += log10(1013.246);
+
+  // factor -3 is to adjust units - Vapor Pressure SVP * humidity
+  double VP = pow(10, RHS - 3) * humidity;
+
+  // (2) DEWPOINT = F(Vapor Pressure)
+  double T = log(VP / 0.61078); // temp var
+  return (241.88 * T) / (17.558 - T);
+}
+
+#include "DHT.h"
+#include "Adafruit_Sensor.h"
+
+#define DHTPIN 2 // what pin we're connected to
+#define DHTTYPE DHT22 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT22 test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hi = dht.computeHeatIndex(f, h);
+  float hiDegC = dht.convertFtoC(hi);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Other Temperature: ");
+  Serial.print(hiDegC);
+  Serial.print(" *C ");
+  Serial.print(hi);
+  Serial.print(" *F ");
+  Serial.print("Dew Point (*C): ");
+  Serial.println(dewPoint(t, h));
+}
+
